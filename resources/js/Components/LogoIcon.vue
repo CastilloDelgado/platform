f513777e7b47e3536566e851b29156460d330026
Marco Castillo <marcocastillo1997@gmail.com>
2023-12-13 15:49:52 -0600

    Adapting navbar

---
+++ resources/js/Components/LogoIcon.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Link href="/">
+        <div class="flex">
+            <p class="font-bold text-2xl mr-1 font-serif">Life & Live Music</p>    
+            <p class="text-xs align-top">[blog]</p>
+        </div>
+    </Link>
+</template>
---
+++ resources/js/Components/NavbarLink.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+defineProps({
+    title: String,
+    href: String,
+    active: Boolean
+})
+
+</script>
+
+<template>
+    <Link 
+        :href="href" 
+        aria-current="page" 
+        class="text-black text-sm border-black pt-2" 
+        :class="{
+            'border-b hover:border-none font-bold': active,
+            'hover:border-b ': !active,
+        }"
+    >
+
+    <span class="pt-2">
+
+        {{ title }} 
+    </span>
+    </Link>
+</template>

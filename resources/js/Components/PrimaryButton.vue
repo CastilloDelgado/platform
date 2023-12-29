60f8d19460f624aa239f657f0ecbc57d6043d818
Marco Castillo <marcocastillo1997@gmail.com>
2023-12-29 13:37:04 -0600

    Text size fixed

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -8,7 +8,8 @@ defineProps({
 </script>
 
 <template>
-    <button :type="type" class="h-10 items-center px-4 py-1 bg-white hover:text-white border-black dark:border-white dark:bg-black border font-semibold text-sm text-black dark:text-white tracking hover:bg-black dark:hover:bg-white  dark:focus:bg-white  dark:hover:text-black dark:hover:border-black ">
+    <button :type="type"
+        class="h-10 items-center text-xs md:text-sm px-4 py-1 bg-white hover:text-white border-black dark:border-white dark:bg-black border font-semibold  text-black dark:text-white tracking hover:bg-black dark:hover:bg-white  dark:focus:bg-white  dark:hover:text-black dark:hover:border-black ">
         <slot />
     </button>
 </template>

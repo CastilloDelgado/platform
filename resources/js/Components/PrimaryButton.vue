69dd9696c93a9a0ef075420a09a4f6e281140e51
Marco Castillo <marcocastillo1997@gmail.com>
2023-12-29 14:01:39 -0600

    Fix on text size in the post badge 'leer mas' button

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
     <button :type="type"
-        class="h-10 items-center text-xs md:text-sm px-4 py-1 bg-white hover:text-white border-black dark:border-white dark:bg-black border font-semibold  text-black dark:text-white tracking hover:bg-black dark:hover:bg-white  dark:focus:bg-white  dark:hover:text-black dark:hover:border-black ">
+        class="h-10 items-center md:px-4 px-2  py-1 bg-white hover:text-white border-black dark:border-white dark:bg-black border font-semibold text-xs md:text-sm text-black dark:text-white tracking hover:bg-black dark:hover:bg-white  dark:focus:bg-white  dark:hover:text-black dark:hover:border-black ">
         <slot />
     </button>
 </template>

99ed0206feb744b534cdc9dd5c99a09dcaa80345
Marco Castillo <marcocastillo1997@gmail.com>
2023-12-31 18:48:39 -0600

    Hiding comment form in poost when user is not logged

--- resources/js/Components/AuthenticationCard.vue
+++ resources/js/Components/AuthenticationCard.vue
@@ -1,7 +1,8 @@
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white dark:bg-black">
+    <div class="max-h-[60hv] flex flex-col sm:justify-center items-center sm:pt-0 bg-white dark:bg-black">
 
-        <div class="w-full sm:max-w-md px-6 py-4 bg-white dark:bg-black shadow-md overflow-hidden sm:rounded-lg">
+        <div
+            class="w-full h-full grid place-content-center sm:max-w-md px-6 py-4 bg-white dark:bg-black shadow-md overflow-hidden sm:rounded-lg">
             <slot name="logo" />
             <slot />
         </div>
--- resources/js/Components/PostCommentsSection.vue
+++ resources/js/Components/PostCommentsSection.vue
@@ -1,6 +1,8 @@
 <script setup>
+import { Link } from '@inertiajs/vue3';
 import PostCommentBadge from './PostCommentBadge.vue';
 import PostCommentForm from './PostCommentForm.vue';
+import PrimaryButton from './PrimaryButton.vue';
 
 defineProps({
     postId: {
@@ -9,13 +11,19 @@ defineProps({
     },
     comments: Array,
 })
+
 </script>
 
 <template>
     <div class="mt-6">
         <p class="font-bold text-lg mb-4">Comentarios</p>
         <div class="space-y-4">
-            <PostCommentForm :post-id="postId" />
+            <PostCommentForm :post-id="postId" v-if="$page.props.auth.user" />
+            <div v-else>
+                <Link :href="route('register')">
+                <PrimaryButton>Ingresa para comentar!</PrimaryButton>
+                </Link>
+            </div>
             <PostCommentBadge v-for="comment in comments" :comment="comment" />
         </div>
     </div>

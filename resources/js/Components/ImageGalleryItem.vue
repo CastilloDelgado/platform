fb6d5684444166601aedd1ed6ba7ea47e50e7977
Marco Castillo <marcocastillo1997@gmail.com>
2024-01-18 00:17:30 -0600

    Updating gallery items

--- resources/js/Components/ImageGalleryItem.vue
+++ resources/js/Components/ImageGalleryItem.vue
@@ -15,14 +15,14 @@ defineProps({
     <Link :href="route('posts.show', image.post.slug)">
     <div class="border border-black dark:border-white relative">
         <img :src="image.image_url"
-            class="transition hover:brightness-50 hover:blur-sm hover:grayscale h-72 w-full object-cover" />
+            class="transition hover:brightness-50 hover:blur hover:grayscale h-72 w-full object-cover" />
         <div
-            class=" absolute top-0 left-0 right-0 bottom-0 grid place-content-center hover:grid hover:bg-black/60 transition text-transparent hover:text-white">
-            <div class="text-center px-12">
-                <p class="font-bold text-xl font-serif">{{ image.post.title }}</p>
-                <p class="text-xs mb-4">¡Haz click para ver más!</p>
-                <p class="capitalize text-xs">creado por</p>
-                <p class="font-md font-bold">{{ image.post.user.name }} </p>
+            class="absolute top-0 left-0 right-0 bottom-0 grid place-content-center hover:grid hover:bg-black/80 bg-black/20 transition text-transparent hover:text-white">
+            <div class="text-center px-2 md:px-12">
+                <p class="font-bold text md:text-xl font-serif">{{ image.post.title }}</p>
+                <p class="text-[8px] md:text-xs mb-4">¡Haz click para ver más!</p>
+                <p class="capitalize text-[8px] md:text-xs">creado por</p>
+                <p class="font-md font-bold text-[11px] md:text-sm">{{ image.post.user.name }} </p>
             </div>
         </div>
     </div>

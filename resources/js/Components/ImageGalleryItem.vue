e1174b4f3c80fba58843f20615b0bae08d27ee34
Marco Castillo <marcocastillo1997@gmail.com>
2024-01-18 00:23:34 -0600

    Fix title size of gallery item

--- resources/js/Components/ImageGalleryItem.vue
+++ resources/js/Components/ImageGalleryItem.vue
@@ -19,7 +19,7 @@ defineProps({
         <div
             class="absolute top-0 left-0 right-0 bottom-0 grid place-content-center hover:grid hover:bg-black/80 bg-black/20 transition text-transparent hover:text-white">
             <div class="text-center px-2 md:px-12">
-                <p class="font-bold text md:text-xl font-serif">{{ image.post.title }}</p>
+                <p class="font-bold text-xs md:text-xl font-serif">{{ image.post.title }}</p>
                 <p class="text-[8px] md:text-xs mb-4">¡Haz click para ver más!</p>
                 <p class="capitalize text-[8px] md:text-xs">creado por</p>
                 <p class="font-md font-bold text-[11px] md:text-sm">{{ image.post.user.name }} </p>

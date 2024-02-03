f377e8f2a3ee1c46de4726125d313bdd2389ac88
Marco Castillo <marcocastillo1997@gmail.com>
2024-02-02 20:19:20 -0600

    Showing latests posts

--- app/Http/Controllers/PublicProfileController.php
+++ app/Http/Controllers/PublicProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -9,9 +10,10 @@ use Inertia\Inertia;
 class PublicProfileController extends Controller
 {
     public function show(User $user){
+        $posts = Post::where('user_id', $user->id)->get();
         return Inertia::render('PublicProfile', [
             'user' => $user,
-            'posts' => $user->posts
+            'posts' => $posts
         ]);
     }
 }

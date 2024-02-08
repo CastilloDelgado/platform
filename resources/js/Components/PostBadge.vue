c3d40d823bc843f063b165f0d7480999d286a742
Marco Castillo <marcocastillo1997@gmail.com>
2024-02-08 16:06:53 -0600

    Adding link to profile in post badge

--- resources/js/Components/PostBadge.vue
+++ resources/js/Components/PostBadge.vue
@@ -14,37 +14,39 @@ defineProps({
 </script>
 
 <template>
-    <Link :href="route('posts.show', post.slug)" as="article"
-        class="border border-black dark:border-white flex flex-col cursor-pointer transition hover:saturate-[1.15]">
-    <div>
+    <article class="border border-black dark:border-white flex flex-col transition hover:saturate-[1.15]">
+        <Link :href="route('posts.show', post.slug)" as="div" class="cursor-pointer">
         <img :src="post.thumbnail_url" alt="Blog Post illustration" class="h-64 w-full object-cover ">
-    </div>
+        </Link>
 
-    <div class="p-6 bg-white dark:bg-black text-black dark:text-white flex flex-col justify-between h-full">
-        <header>
-            <div class="space-x-2">
-                <CategoryBadge>{{ post.category.name }}</CategoryBadge>
-            </div>
+        <div class="p-6 bg-white dark:bg-black text-black dark:text-white flex flex-col justify-between h-full">
+            <header>
+                <div class="space-x-2">
+                    <CategoryBadge>{{ post.category.name }}</CategoryBadge>
+                </div>
 
-            <div class="mt-4">
-                <h1 class="text-2xl font-serif font-bold">
-                    {{ post.title }}
-                </h1>
+                <Link :href="route('posts.show', post.slug)" class="cursor-pointer ">
+                <div class="mt-4">
+                    <h1 class="text-2xl font-serif font-bold">
+                        {{ post.title }}
+                    </h1>
 
-                <span class="mt-2 block text-gray-400 text-xs">
-                    Published <time>{{ formatDistance(new Date(post.created_at), new Date(), { addSuffix: true }) }}</time>
-                </span>
-            </div>
-        </header>
+                    <span class="mt-2 block text-gray-400 text-xs">
+                        Published <time>{{ formatDistance(new Date(post.created_at), new Date(), { addSuffix: true })
+                        }}</time>
+                    </span>
+                </div>
+                </Link>
+            </header>
 
-        <div class="text-sm mt-4">
+            <Link :href="route('posts.show', post.slug)" class="text-sm mt-4 cursor-pointer" as="div">
             <p class="text-justify">
                 {{ post.excerpt }}
             </p>
-        </div>
+            </Link>
 
-        <footer class="flex justify-between items-center mt-8">
-            <div class="flex items-center text-sm">
+            <footer class="flex justify-between items-center mt-8">
+                <Link as="div" class="flex items-center text-sm cursor-pointer" :href="route('profile.show', post.user.id)">
                 <img :src="post.user.profile_photo_url" class="w-12 h-12 object-cover border border-black dark:border-white"
                     alt="Lary avatar">
                 <div class="ml-3">
@@ -54,14 +56,14 @@ defineProps({
                             true
                     }) }}</p>
                 </div>
-            </div>
-
-            <div>
-                <Link :href="route('posts.show', post.slug)">
-                <PrimaryButton>Leer más</PrimaryButton>
                 </Link>
-            </div>
-        </footer>
-    </div>
-    </Link>
+
+                <div>
+                    <Link :href="route('posts.show', post.slug)" class="cursor-pointer">
+                    <PrimaryButton>Leer más</PrimaryButton>
+                    </Link>
+                </div>
+            </footer>
+        </div>
+    </article>
 </template>

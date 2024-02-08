c6d4c92589f15ccbee0be36fb228be870daf0c83
Marco Castillo <marcocastillo1997@gmail.com>
2024-02-08 16:21:49 -0600

    Reducing gap in RandomImageGallery container2 2

--- resources/js/Components/RandomImageGallery.vue
+++ resources/js/Components/RandomImageGallery.vue
@@ -12,12 +12,12 @@ defineProps({
 
 <template>
     <div class="text-white mb-8">
-        <div class="hidden md:grid grid-cols-4 gap-2">
+        <div class="hidden md:grid grid-cols-4 gap-[4px]">
             <ImageGalleryItem v-for="(image, index) in images" :image="image" :class="{
                 'col-span-2': [1, 3, 8].find((el) => el === index)
             }" />
         </div>
-        <div class="grid md:hidden grid-cols-3 md:grid-cols-4 gap-2">
+        <div class="grid md:hidden grid-cols-3 md:grid-cols-4 gap-[4px]">
             <ImageGalleryItem v-for="(image, index) in images.slice(0, 8)" :image="image" :class="{
                 'col-span-2': [1, 2, 5, 6].find((el) => el === index)
             }" />

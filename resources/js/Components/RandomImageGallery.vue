96d6dc075abc25c1953e336c5208f3b65c593e9f
Marco Castillo <marcocastillo1997@gmail.com>
2024-04-28 20:54:50 -0600

    Improve in custom image gallery for mobile

--- resources/js/Components/RandomImageGallery.vue
+++ resources/js/Components/RandomImageGallery.vue
@@ -18,8 +18,8 @@ defineProps({
             }" />
         </div>
         <div class="grid md:hidden grid-cols-3 md:grid-cols-4 gap-[4px]">
-            <ImageGalleryItem v-for="(image, index) in images.slice(0, 8)" :image="image" :class="{
-                'col-span-2': [1, 2, 5, 6, 7, 8, 11, 12, 13, 14, 17].find((el) => el === index)
+            <ImageGalleryItem v-for="(image, index) in images" :image="image" :class="{
+                'col-span-2': [1, 2, 5, 6, 9, 10, 13, 14, 17, 18].find((el) => el === index)
             }" />
         </div>
     </div>
